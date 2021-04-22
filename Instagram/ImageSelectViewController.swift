a9677284f87523805bd64f9bfc5dcd4b8863bba0
peiwen.ryuu <liu@i-enter.co.jp>
2021-04-22 11:51:12 +0900

    ImageSelectViewControllerに画像加工済みとキャセル処理完成した

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import CLImageEditor
 
-class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ImageSelectViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,20 +41,56 @@ class ImageSelectViewController: UIViewController, UIImagePickerControllerDelega
         self.dismiss(animated: true, completion: nil)
     }
     
-    // MARK: - デリゲートメソッド
-    
-    //写真を撮影/選択した時に呼ばれるメソッド
+}
+
+// MARK: - Extension
+// UIImagePickerControllerデリゲート
+extension ImageSelectViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    /// 写真を撮影/選択した時に呼ばれるメソッド
+    /// - Parameters:
+    ///   - picker: 選択コントローラー
+    ///   - info: 選択した画像情報
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        //撮影/選択された画像を取得する
+        // 撮影/選択された画像を取得する
         let image = info[.originalImage] as! UIImage
         
-        //後でCLImageEditorライブラリで加工する
+        // 後でCLImageEditorライブラリで加工する
         print("DEBUG_PRINT: image = \(image)")
+        // CLImageEditorにimageを渡して、加工画面を起動する
+        let editor = CLImageEditor(image: image)!
+        editor.delegate = self
+        editor.modalPresentationStyle = .fullScreen
+        picker.present(editor, animated: true, completion: nil)
     }
     
+    /// キャンセルをタップした時に呼ばれるメソッド
+    /// - Parameter picker: 選択コントローラー
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        //ImageSelectViewController画面を閉じてタブ画面に戻る
+        // ImageSelectViewController画面を閉じてタブ画面に戻る
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
 }
 
+// CLImageEditorライブラリ
+extension ImageSelectViewController: CLImageEditorDelegate {
+
+    /// CLImageEditorで加工が終わった時に呼ばれるメソッド
+    /// - Parameters:
+    ///   - editor: CLImageEditor
+    ///   - image: 加工済み画像
+    func imageEditor(_ editor: CLImageEditor!, didFinishEditingWith image: UIImage!) {
+        // 投稿画面を開く
+        let postViewController = self.storyboard?.instantiateViewController(withIdentifier: "Post") as! PostViewController
+        postViewController.image = image!
+        editor.present(postViewController, animated: true, completion: nil)
+    }
+    
+    /// CLImageEditorの編集がキャンセルされた時に呼ばれるメソッド
+    /// - Parameter editor: CLImageEditor
+    func imageEditorDidCancel(_ editor: CLImageEditor!) {
+        // ImageSelectViewController画面を閉じてタブ画面に戻る
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+
+}
--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -6,24 +6,29 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class PostViewController: UIViewController {
+    
+    var image: UIImage!
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 投稿ボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handlePostButton(_ sender: Any) {
     }
-    */
+    
+    // キャンセルボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handleCancelButton(_ sender: Any) {
+    }
+    
 
 }
