11d5201a539bf0cfaa3346f7331dfc3478cea746
peiwen.ryuu <liu@i-enter.co.jp>
2021-04-22 13:34:29 +0900

    投稿機能を完成した

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Liu Peiwen on 2021/04/22.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
